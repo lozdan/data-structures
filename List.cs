e7a6d32affe026edf07e9afb273349804a22b54e
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-12 15:34:56 -0500

    add more functions

--- List.cs
+++ List.cs
@@ -1,14 +1,29 @@
-/*
+*
 author: Daniel Lozano
 */
 
 namespace MyDataStructures
 {
-
     public class MyList
     {
-        public int[] numbers;
-        public int len;
+        private int[] numbers;
+        private int len;
+
+        public int Length
+        {
+            get
+            {
+                return len;
+            }
+        }
+
+        public int[] Numbers
+        {
+            get
+            {
+                return numbers;
+            }
+        }
 
         public MyList()
         {
@@ -23,12 +38,12 @@ namespace MyDataStructures
             this.numbers = new int[this.len + 1];
 
             this.numbers[this.len] = value;
-            
+
             for (int i = 0; i < this.len; i++)
             {
                 this.numbers[i] = temp_numbers[i];
             }
-            this.len++;                          
+            this.len++;
         }
 
         public void Insert(int value, int index)
@@ -37,7 +52,7 @@ namespace MyDataStructures
             this.numbers = new int[this.len + 1];
 
             int count = 0;
-            for(int i = 0; i <= this.len; i++)
+            for (int i = 0; i <= this.len; i++)
             {
                 if (i == index)
                 {
@@ -59,7 +74,7 @@ namespace MyDataStructures
             this.numbers = new int[this.len - 1];
 
             int count = 0;
-            for(int i = 0; i < len - 1; i++)
+            for (int i = 0; i < len - 1; i++)
             {
                 if (i == index)
                 {
@@ -72,6 +87,60 @@ namespace MyDataStructures
             }
             len--;
         }
-    }    
+
+        public bool Contains(int value)
+        {
+            bool exist = false;
+            for (int i = 0; i < this.len && exist is false; i++)
+            {
+                if (this.numbers[i] == value)
+                    exist = true;
+            }
+            return exist;
+        }
+
+        public void AddRange(int[] sequence)
+        {
+            int[] temp_numbers = this.numbers;
+            this.numbers = new int[this.len + sequence.Length];
+            this.len = this.len + sequence.Length;
+
+            int j = 0;
+            for (int i = 0; i < this.len; i++)
+            {
+                if (i < temp_numbers.Length)
+                    numbers[i] = temp_numbers[i];
+                else
+                {
+                    numbers[i] = sequence[j];
+                    j++;
+                }
+            }
+        }
+
+        public void RemoveAll(int value)
+        {
+            int repeat_numbers = 0;
+            for(int i = 0; i < this.len; i++)
+            {
+                if (this.numbers[i] == value)
+                    repeat_numbers++;
+            }
+
+            int[] temp_numbers = this.numbers;
+            this.len = this.len - repeat_numbers;
+            this.numbers = new int[this.len];
+
+            int count = 0;
+            for(int i = 0; i < temp_numbers.Length; i++)
+            {
+                if (temp_numbers[i] != value)
+                {
+                    this.numbers[count] = temp_numbers[i];
+                    count++;
+                }
+            }
+        }
+    }
 }
 
